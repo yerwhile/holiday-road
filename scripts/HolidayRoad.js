b7f63edfb4f79eefba84aa685a26a1377ab84309
Matthew McDonald <yerwhile@gmail.com>
2023-01-05 14:40:39 -0600

    beginnings

---
+++ scripts/HolidayRoad.js
@@ -0,0 +1,30 @@
+export const HolidayRoad = () => {
+    return `
+    <div id="logo">LOGO</div>
+    <div id="selectBoxes">
+        <select name="parks" id="parkSelect">
+            <option value="">Choose Option</option>
+        </select>
+        <select name="bizarrerie" id="bizarrerieSelect">
+            <option value="">Choose Option</option>
+        </select>
+        <select name="eateries" id="eateriesSelect">
+            <option value="">Choose Option</option>
+        </select>
+    </div>
+    <div id="itineraries">
+        <div id="itineraryPreview">
+            Itinerary Choice One
+            Itinerary Choice Two
+        </div>
+        <div id="savedItineraries">
+            Saved Itinerary One
+            Saved Itinerary Two
+            Saved Itinerary Three
+        </div>
+    </div>
+    <div id="weatherForecast">
+        Weather Forecast
+    </div>
+    </div>`
+}
