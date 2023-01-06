5cdbfa8b1538322ee29c7d22813b1a172f5a5b91
Logan Gray <loganallengray@gmail.com>
2023-01-06 11:44:38 -0600

    Parks now display, new event listener

--- scripts/ItineraryPreview.js
+++ scripts/ItineraryPreview.js
@@ -1,6 +1,15 @@
 import { getData, applicationState } from "./dataAccess.js";
 
 export const ItineraryPreview = () => {
+    const parkData = getData("parks");
+    const parks = parkData.data;
+    const selectedPark = parks.find(park => park.id === applicationState.chosenPark);
+    let selectedParkName = "";
+
+    if (applicationState.chosenPark !== undefined) {
+        selectedParkName = selectedPark.fullName;
+    }
+
     const attractions = getData("attractions");
     let selectedAttractionName = "";
     for(const attraction of attractions) {
@@ -8,5 +17,7 @@ export const ItineraryPreview = () => {
             selectedAttractionName = attraction.name;
         }
     }
-    return `<div id="attractionPreview">Selected Attraction: ${selectedAttractionName}</div>`
+    return `
+        <div id="parkPreview">Selected Park: ${selectedParkName}</div>
+        <div id="attractionPreview">Selected Attraction: ${selectedAttractionName}</div>`
 }
