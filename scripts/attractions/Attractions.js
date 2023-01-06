2e1e508cd1a8f9c51edcd2c60efe1de9c83b407d
Matthew McDonald <yerwhile@gmail.com>
2023-01-06 09:16:36 -0600

    attractions selectbox

---
+++ scripts/attractions/Attractions.js
@@ -0,0 +1,24 @@
+import { getData, mainContainer, applicationState } from "../dataAccess.js";
+
+export const Attractions = () => {
+    const attractions = getData("attractions");
+
+    return `
+    <label class="label" for="attractionsSelect">Attractions</label>
+    <select name="attractionSelect" id="attractions">
+        <option value="0">Choose Recipient</option>
+        ${attractions.map(
+            attraction => {
+                return `<option value="${attraction.id}">${attraction.name}</option>`
+            }
+        ).join("")}
+    </select>`
+}
+
+mainContainer.addEvenListener(
+    "change",
+    (event) => {
+        if(event.target.id === "attractions") {
+            applicationState.chosenAttraction === document.querySelector("select[name='attractionSelect']").value
+        }
+    })
