cf652d0157fad0792ace9679698f16b727161a87
Shane <mhurtado1@berklee.edu>
2023-01-06 09:27:40 -0600

    shanes stuff

---
+++ scripts/eateries/Eatery.js
@@ -0,0 +1,18 @@
+import {} from "./.js"
+
+export const Eatery = () => {
+    const eateries = getEatery()
+
+    let html = ""
+    html += '<select id="eatery">'
+    html += '<option value="0"></option>'
+    const eateryArray = eateries.map(
+        eatery => {
+            return `<option value="${eatery.id}">${eatery.businessName}</option>`
+        }
+    )
+    html +=eateryArray .join("")
+    html += "</select>"
+    return html
+
+}
