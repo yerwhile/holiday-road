f35aef4647532057b2bbc092cf4b08451946f114
Logan Gray <loganallengray@gmail.com>
2023-01-09 10:23:40 -0600

    added weather fetch

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-import { fetchData, fetchForeignData, localAPI } from "./dataAccess.js"
+import { applicationState, fetchData, fetchForeignData, getData } from "./dataAccess.js"
 import { fetchEateries } from "./eateries/EateryProvider.js"
 import { HolidayRoad } from "./HolidayRoad.js"
 import Settings from "./Settings.js"
@@ -40,6 +40,12 @@ mainContainer.addEventListener(
 mainContainer.addEventListener(
     "parkSelected",
     customEvent => {
+        const parks = getData("parks").data;
+        const selectedPark = parks.find(park => park.id === applicationState.chosenPark);
+        const lat = selectedPark.latitude;
+        const lon = selectedPark.longitude;
+        fetchForeignData(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${Settings.weatherKey}`, "weather");
+
         document.querySelector('#attractionsSelect').innerHTML = Attractions();
         document.querySelector('#eaterySelect').innerHTML = Eateries();
     }
