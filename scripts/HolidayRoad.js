6c44b463f173558435335c5a3acc648fd3490257
Shane <mhurtado1@berklee.edu>
2023-01-10 09:17:59 -0600

    fixed alt

--- scripts/HolidayRoad.js
+++ scripts/HolidayRoad.js
@@ -10,7 +10,7 @@ import { Weather } from "./weather/weather.js"
 
 export const HolidayRoad = () => {
     return `
-    <div id="logo"><img src="https://media.istockphoto.com/id/1319082592/vector/round-tree-icon-with-leaves-and-roots.jpg?s=612x612&w=0&k=20&c=N3tHnScT5KmcODhCgrYEvM5bEsJOR66oMAiS2UYj9MU=" alt="Buttons and Lollipop" width="100px">
+    <div id="logo"><img src="https://media.istockphoto.com/id/1319082592/vector/round-tree-icon-with-leaves-and-roots.jpg?s=612x612&w=0&k=20&c=N3tHnScT5KmcODhCgrYEvM5bEsJOR66oMAiS2UYj9MU=" alt="Logo For Old Yeller's Trip Planner" width="100px">
     <h2>Old Yeller's Trip Planner</h2>
     </div>
     <div id="selectBoxes">
